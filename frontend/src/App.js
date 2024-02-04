13e064953c2b8a281767a598ccdc1b3dae88e7d5
kells\KellsPing <kellsping@gmail.com>
2024-02-04 14:47:18 +0800

    [Feat.] Create file and add to route.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ function App() {
             <Route path="" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/create" element={<Create />} />
+            <Route path="/edit/:id" element={<Create />} />
           </Routes>
         }
       />
